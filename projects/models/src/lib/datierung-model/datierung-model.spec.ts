e7a69958b2dcfe37bd61fab41b8515388ca071bf
Jakob Vesely <jakob@appartig.at>
2020-12-08 21:17:23 +0100

    Add first Model

---
+++ projects/models/src/lib/datierung-model/datierung-model.spec.ts
@@ -0,0 +1,9 @@
+import { DatierungModel } from './datierung-model';
+
+describe('DatierungModel', () =>
+{
+    it('should create an instance', () =>
+    {
+        expect(new DatierungModel()).toBeTruthy();
+    });
+});
---
+++ projects/models/src/lib/datierung-model/datierung-model.ts
@@ -0,0 +1,6 @@
+export class DatierungModel
+{
+    public von: string | Date;
+    public bis: string | Date;
+    public text: string;
+}
