243c00ef5b5051b86aecaee773bd227af5efe500
Jakob Vesely <jakob@appartig.at>
2020-12-12 13:40:11 +0100

    Fix Path/Entry Point

--- projects/data/src/public-api.ts
+++ projects/data/src/public-api.ts
@@ -1,7 +1,34 @@
-/*
- * Public API Surface of data
- */
-
-export * from './lib/data.service';
-export * from './lib/data.component';
-export * from './lib/data.module';
+export { ArbeitslisteItemEintragModel } from './lib/arbeitsliste-item-eintrag-model/arbeitsliste-item-eintrag-model';
+export { ArbeitslisteModel } from './lib/arbeitsliste-model/arbeitsliste-model';
+export { BehaeltnisModel } from './lib/behaeltnis-model/behaeltnis-model';
+export { BestandModel } from './lib/bestand-model/bestand-model';
+export { DatierungADModel } from './lib/datierung-ad-model/datierung-ad-model';
+export { DatierungModel } from './lib/datierung-model/datierung-model';
+export { DatumADModel } from './lib/datum-ad-model/datum-ad-model';
+export { EinschraenkungenModel } from './lib/einschraenkungen-model/einschraenkungen-model';
+export { EintragListModel } from './lib/eintrag-list-model/eintrag-list-model';
+export { EintragModel } from './lib/eintrag-model/eintrag-model';
+export { EreignisModel } from './lib/ereignis-model/ereignis-model';
+export { ExternerBesitzerModel } from './lib/externer-besitzer-model/externer-besitzer-model';
+export { FachModel } from './lib/fach-model/fach-model';
+export { FarbraumModel } from './lib/farbraum-model/farbraum-model';
+export { ItemDigitalModel } from './lib/item-digital-model/item-digital-model';
+export { ItemExternModel } from './lib/item-extern-model/item-extern-model';
+export { ItemInternModel } from './lib/item-intern-model/item-intern-model';
+export { ItemsModel } from './lib/items-model/items-model';
+export { KastenModel } from './lib/kasten-model/kasten-model';
+export { KontextualisierungModel } from './lib/kontextualisierung-model/kontextualisierung-model';
+export { MediumHauptkategorieModel } from './lib/medium-hauptkategorie-model/medium-hauptkategorie-model';
+export { MediumUnterkategorieModel } from './lib/medium-unterkategorie-model/medium-unterkategorie-model';
+export { OriginalitaetModel } from './lib/originalitaet-model/originalitaet-model';
+export { PersonModel } from './lib/person-model/person-model';
+export { PriorisierungModel } from './lib/priorisierung-model/priorisierung-model';
+export { SearchResultItemModel } from './lib/search-result-item-model/search-result-item-model';
+export { SelectsModel } from './lib/selects-model/selects-model';
+export { SerieModel } from './lib/serie-model/serie-model';
+export { TagModel } from './lib/tag-model/tag-model';
+export { TagsModel } from './lib/tags-model/tags-model';
+export { ThemaHauptkategorieModel } from './lib/thema-hauptkategorie-model/thema-hauptkategorie-model';
+export { ThemaUnterkategorieModel } from './lib/thema-unterkategorie-model/thema-unterkategorie-model';
+export { UploadModel } from './lib/upload-model/upload-model';
+export { VerortungModel } from './lib/verortung-model/verortung-model';
