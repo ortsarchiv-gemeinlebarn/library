365abb9a8649e78804d826978302d7927e2e29b7
Jakob Vesely <jakob@appartig.at>
2021-01-25 23:52:11 +0100

    Fix for Thema Hauptkategorie Parent Name (= Bereich)

--- projects/data/src/lib/thema-hauptkategorie-model/thema-hauptkategorie-model.ts
+++ projects/data/src/lib/thema-hauptkategorie-model/thema-hauptkategorie-model.ts
@@ -2,7 +2,7 @@ import { ThemaBereichModel } from '../thema-bereich-model/thema-bereich-model';
 export class ThemaHauptkategorieModel
 {
     public id: number;
-    public bestand: ThemaBereichModel;
+    public bereich: ThemaBereichModel;
     public titel: string;
     public abkuerzung: string;
     public sortIndex: number;
