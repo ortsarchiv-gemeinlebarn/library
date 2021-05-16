79dd50cb9e774a4d7a4df018372d4d75d1228f13
Jakob Vesely <jakob@appartig.at>
2021-05-16 17:29:35 +0200

    extend ereignis mit ThemaBereich

--- projects/data/src/lib/ereignis-model/ereignis-model.ts
+++ projects/data/src/lib/ereignis-model/ereignis-model.ts
@@ -3,6 +3,7 @@ import { EintragListModel } from '../eintrag-list-model/eintrag-list-model';
 import { KontextualisierungModel } from '../kontextualisierung-model/kontextualisierung-model';
 import { PersonModel } from '../person-model/person-model';
 import { PriorisierungModel } from '../priorisierung-model/priorisierung-model';
+import { ThemaBereichModel } from '../thema-bereich-model/thema-bereich-model';
 import { ThemaHauptkategorieModel } from '../thema-hauptkategorie-model/thema-hauptkategorie-model';
 import { ThemaUnterkategorieModel } from '../thema-unterkategorie-model/thema-unterkategorie-model';
 
@@ -15,6 +16,7 @@ export class EreignisModel
     public quellen: string = null;
     public kontextualisierung: KontextualisierungModel = null;
     public priorisierung: PriorisierungModel = null;
+    public themaBereich: ThemaBereichModel = null;
     public themaHauptkategorie: ThemaHauptkategorieModel = null;
     public themaUnterkategorie: ThemaUnterkategorieModel = null;
     public personen: Array<PersonModel> = [];
